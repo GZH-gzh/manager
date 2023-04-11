1f2dcb7359ac35de8564c5ec5441fbebb335e3da
g-zehong <869401376@qq.com>
2023-04-11 11:33:21 +0800

    修改

--- manager-fe/src/config/index.js
+++ manager-fe/src/config/index.js
@@ -5,15 +5,15 @@ const env = import.meta.env.MODE || 'prod';
 const EnvConfig = {
     dev:{
         baseApi:'/api',
-        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+        mockApi:'https://www.fastmock.site/mock/api'
     },
     test:{
         baseApi:'//test.futurefe.com/api',
-        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+        mockApi:'https://www.fastmock.site/mock/api'
     },
     prod:{
         baseApi:'//futurefe.com/api',
-        mockApi:'https://www.fastmock.site/mock/c1c302e8baed9894c48c17e4738c092e/api'
+        mockApi:'https://www.fastmock.site/mock/api'
     }
 }
 export default {
